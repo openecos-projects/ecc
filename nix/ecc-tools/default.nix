8277fa3f1bc50a6e9f55e2ce0fddca60d6cc284b
Emin <me@emin.chat>
2026-03-12 17:14:24 +0800

    fix(nix): enable installation phase for shared object files
    Signed-off-by: Emin <me@emin.chat>

--- nix/ecc-tools/default.nix
+++ nix/ecc-tools/default.nix
@@ -113,9 +113,6 @@ stdenv.mkDerivation {
     tbb_2022
   ];
 
-  # Don't run install phase, we only need the .so file
-  dontInstall = true;
-
   postInstall = ''
     # Tests rely on hardcoded path, so they should not be included
     rm $out/bin/*test $out/bin/*Test $out/bin/test_* $out/bin/*_app
