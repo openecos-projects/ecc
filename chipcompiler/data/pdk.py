894823b13289fe9e75364ac19b4b25d3c58788e6
TAOUINAR ABDELBASSET <trabdelbasset@gmail.com>
2026-04-12 23:34:47 +0100

    feat(pdk): add IHP SG13G2 130nm open-source PDK support

--- chipcompiler/data/pdk.py
+++ chipcompiler/data/pdk.py
@@ -56,6 +56,8 @@ def get_pdk(pdk_name : str, pdk_root: str = "") -> PDK:
     pdk_name_normalized = (pdk_name or "").strip().lower()
     if pdk_name_normalized == "ics55":
         pdk = PDK_ICS55(pdk_root=pdk_root)
+    elif pdk_name_normalized == "sg13g2":
+        pdk = PDK_SG13G2(pdk_root=pdk_root)
     else:
         pdk = PDK(name=pdk_name_normalized)
     pdk.validate()
@@ -128,6 +130,56 @@ def PDK_ICS55(pdk_root: str = "") -> PDK:
             "*OAI33*",
             "*NOR4*",
             "ICG*"
+        ]
+    )
+
+    return pdk
+
+def PDK_SG13G2(pdk_root: str = "") -> PDK:
+    resolved_root = os.path.abspath(os.path.expanduser(
+        (pdk_root or "").strip()
+        or os.environ.get("CHIPCOMPILER_SG13G2_PDK_ROOT", "").strip()
+        or os.environ.get("SG13G2_PDK_ROOT", "").strip()
+    ))
+
+    tech_path = "{}/lef/sg13g2_tech.lef".format(resolved_root)
+    lef_paths = [
+        "{}/lef/sg13g2_stdcell.lef".format(resolved_root)
+    ]
+    lib_paths = [
+        "{}/lib/sg13g2_stdcell_typ_1p20V_25C.lib".format(resolved_root)
+    ]
+
+    pdk = PDK(
+        name="sg13g2",
+        version="1.0",
+        root=resolved_root,
+        tech=tech_path if os.path.isfile(tech_path) else "",
+        lefs=[path for path in lef_paths if os.path.isfile(path)],
+        libs=[path for path in lib_paths if os.path.isfile(path)],
+        site_core="CoreSite",
+        buffers=[
+            "sg13g2_buf_1",
+            "sg13g2_buf_2",
+            "sg13g2_buf_4",
+            "sg13g2_buf_8",
+            "sg13g2_buf_16"
+        ],
+        fillers=[
+            "sg13g2_fill_1",
+            "sg13g2_fill_2",
+            "sg13g2_decap_4",
+            "sg13g2_decap_8"
+        ],
+        tie_high_cell="sg13g2_tiehi",
+        tie_high_port="L_HI",
+        tie_low_cell="sg13g2_tielo",
+        tie_low_port="L_LO",
+        dont_use=[
+            "sg13g2_lgcp_1",
+            "sg13g2_sighold",
+            "sg13g2_slgcp_1",
+            "sg13g2_dfrbp_2"
         ]
     )
 
