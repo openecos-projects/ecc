29623c17fef64852888ccc7782a254f5d171d199
Anillc <void@anil.lc>
2026-05-21 16:02:40 +0800

    fix(nix): package ecc-tools from vendored Rust deps (#83)

--- nix/python/ecc-tools/default.nix
+++ nix/python/ecc-tools/default.nix
@@ -4,6 +4,8 @@
   buildPythonPackage,
   fetchFromGitHub,
   callPackages,
+  rustPlatform,
+  cargo,
   cmake,
   ninja,
   flex,
@@ -26,91 +28,74 @@
   onnxruntime,
   tbb_2022,
   uv-build,
+  autoPatchelfHook,
 }:
 
 let
   version = "0.1.0-alpha.2";
 
-  rootSrc = fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "openecos-projects";
     repo = "ecc-tools";
-    rev = "36160db0b30ccd627f2c2a06d9fa517d4cce4d49";
-    hash = "sha256-/09acQVPB9l4EyWtKy3DGkIFsjsJkao2PW3VS2gmLLI=";
+    rev = "3bbe522f92c76c74de9c2d2dc5e338fa317c787d";
+    hash = "sha256-tgK3NB1zS3JIAZZEIG6JCbIz8nj/QBlFogpGj3Ty5DY=";
   };
 
-  patchedSrc = stdenv.mkDerivation {
-    pname = "ecc-tools-src";
-    inherit version;
-    src = rootSrc;
+  installDeps = lib.pipe {
+    iir-rust = "src/operation/iIR/source/iir-rust/iir";
+    liberty-parser = "src/database/manager/parser/liberty/lib-rust/liberty-parser";
+    sdf_parser = "src/database/manager/parser/sdf/sdf_parse";
+    spef-parser = "src/database/manager/parser/spef/spef-parser";
+    vcd_parser = "src/database/manager/parser/vcd/vcd_parser";
+    verilog-parser = "src/database/manager/parser/verilog/verilog-rust/verilog-parser";
+  } [
+    (lib.mapAttrsToList (name: path: ''
+      mkdir -p ${path}/.cargo
+      cat <<EOF > ${path}/.cargo/config.toml
+      [source."crates-io"]
+      "replace-with" = "vendored-sources"
 
-    patches = [
-      ./use-nix-built-rust-libraries.patch
-      ./fix-ino-output-summary-init.patch
-    ];
-
-    postPatch = ''
-      substituteInPlace src/operation/iIR/source/iir-rust/CMakeLists.txt \
-        --replace-fail 'ADD_EXTERNAL_PROJ(iir)' "" \
-        --replace-fail 'target_link_libraries(iIR-Rust PRIVATE ''${RUST_LIB_PATH} dl)' 'target_link_libraries(iIR-Rust PRIVATE iir dl)'
-
-      substituteInPlace src/operation/iSTA/CMakeLists.txt \
-        --replace-fail 'link_directories(''${HOME_THIRDPARTY}/onnxruntime/)' 'link_libraries(${onnxruntime}/lib/libonnxruntime.so)'
-    '';
-
-    dontBuild = true;
-    dontFixup = true;
-
-    installPhase = ''
-      runHook preInstall
-      cp -r . "$out"
-      runHook postInstall
-    '';
-  };
-
-  rustpkgs = callPackages ./rustpkgs.nix { rootSrc = patchedSrc; };
-
-  nativeInputs = [
-    cmake
-    ninja
-    flex
-    bison
-    python
-    patchelf
-    pkg-config
-  ];
-
-  runtimeInputs = [
-    rustpkgs.iir-rust
-    rustpkgs.sdf_parse
-    rustpkgs.spef-parser
-    rustpkgs.vcd_parser
-    rustpkgs.verilog-parser
-    rustpkgs.liberty-parser
-    stdenv.cc.cc.lib
-    zlib
-    tcl
-    boost
-    eigen
-    yaml-cpp
-    libunwind
-    glog
-    gtest
-    gflags
-    metis
-    gmp
-    curl
-    onnxruntime
-    tbb_2022
+      [source."vendored-sources"]
+      "directory" = "${rustPlatform.importCargoLock {
+        lockFile = "${src}/${path}/Cargo.lock";
+      }}"
+      EOF
+    ''))
+    (lib.concatStringsSep "\n")
   ];
 
   runtime = stdenv.mkDerivation {
     pname = "ecc-tools-runtime";
-    inherit version;
-    src = patchedSrc;
+    inherit version src;
 
-    nativeBuildInputs = nativeInputs;
-    buildInputs = runtimeInputs;
-    cmakeGenerator = "Ninja";
+    nativeBuildInputs = [
+      cmake
+      ninja
+      flex
+      bison
+      python
+      patchelf
+      pkg-config
+      cargo
+      autoPatchelfHook
+    ];
+
+    buildInputs = [
+      stdenv.cc.cc.lib
+      zlib
+      tcl
+      boost
+      eigen
+      yaml-cpp
+      libunwind
+      glog
+      gtest
+      gflags
+      metis
+      gmp
+      curl
+      tbb_2022
+    ];
 
     cmakeFlags = [
       (lib.cmakeBool "BUILD_ECOS" true)
@@ -119,7 +104,10 @@ let
       (lib.cmakeBool "COMPATIBILITY_MODE" true)
       (lib.cmakeFeature "Python3_EXECUTABLE" python.interpreter)
       (lib.cmakeFeature "Python3_ROOT_DIR" "${python}")
+      "-GNinja"
     ];
+
+    postPatch = installDeps;
 
     buildPhase = ''
       runHook preBuild
@@ -130,59 +118,33 @@ let
     installPhase = ''
       runHook preInstall
 
-      install -d "$out/ecc_tools_bin"
-      for dir in . ../bin; do
-        if [ -d "$dir" ]; then
-          find "$dir" -type f -name '*.so*' -exec cp -f {} "$out/ecc_tools_bin/" \;
-        fi
-      done
+      pushd ..
+      mkdir -p $out/ecc_tools_bin
+      find . -type f -executable -name '*.so*' -exec cp -f {} "$out/ecc_tools_bin/" \;
 
-      ecc_py_so="$(find "$out/ecc_tools_bin" -type f -name 'ecc_py*.so' -print -quit)"
-      if [ -z "$ecc_py_so" ]; then
-        echo "ERROR: ecc_py extension was not built" >&2
-        exit 1
-      fi
-
-      for so in "$out"/ecc_tools_bin/*.so*; do
-        [ -e "$so" ] || continue
-        patchelf --set-rpath "\$ORIGIN:${lib.makeLibraryPath runtimeInputs}" "$so" || true
+      for so in "$out"/ecc_tools_bin/*; do
+        patchelf --set-rpath "" "$so" || true
       done
+      popd
 
       runHook postInstall
     '';
 
-    enableParallelBuild = true;
+    enableParallelBuilding = false;
   };
 in
 buildPythonPackage {
   pname = "ecc-tools";
-  inherit version;
+  inherit version src;
   pyproject = true;
 
-  src = patchedSrc;
-
-  buildInputs = runtimeInputs;
-
   build-system = [ uv-build ];
-  nativeBuildInputs = [ patchelf ];
-
-  preBuild = ''
-    install -d ecc_tools_bin
-    cp -f ${runtime}/ecc_tools_bin/*.so* ecc_tools_bin/
-  '';
 
   postInstall = ''
     site_packages="$out/${python.sitePackages}"
-    install -d "$site_packages/ecc_tools_bin"
-    cp -f ${patchedSrc}/ecc_tools_bin/__init__.py "$site_packages/ecc_tools_bin/"
-    cp -f ${runtime}/ecc_tools_bin/*.so* "$site_packages/ecc_tools_bin/"
-  '';
-
-  postFixup = ''
-    for so in "$out/${python.sitePackages}"/ecc_tools_bin/*.so*; do
-      [ -e "$so" ] || continue
-      patchelf --set-rpath "\$ORIGIN:${lib.makeLibraryPath runtimeInputs}" "$so"
-    done
+    mkdir -p "$site_packages/ecc_tools_bin"
+    cp -f ${src}/ecc_tools_bin/__init__.py "$site_packages/ecc_tools_bin/"
+    ln -s ${runtime}/ecc_tools_bin/ecc_py.*.so "$site_packages/ecc_tools_bin/"
   '';
 
   doCheck = false;
