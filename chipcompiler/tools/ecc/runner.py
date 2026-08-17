4875ce8a2b9b56234f0f862d3806004694be5661
Yell-walkalone <12112088@qq.com>
2026-08-17 10:22:45 +0800

    style: format runner.py with ruff

--- chipcompiler/tools/ecc/runner.py
+++ chipcompiler/tools/ecc/runner.py
@@ -598,7 +598,6 @@ def run_cts(workspace: Workspace, step: EccStep, ecc_module: ECCToolsModule | No
 
         reslut = save_data(workspace=workspace, step=step, ecc_module=ecc_module)
 
-
         sub_flow.update_step(step_name=EccSubFlowEnum.save_data.value, state=StateEnum.Success)
 
         run_analysis(workspace=workspace, step=step, subflow=sub_flow)
